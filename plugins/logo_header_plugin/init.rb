9ee6a8b4628cd3a1af42356304170a325e4b10f9
noedem <noe.demelchiorre@gmail.com>
2016-06-22 15:34:18 -0300

    proyecto final subido

---
+++ plugins/logo_header_plugin/init.rb
@@ -0,0 +1,6 @@
+Redmine::Plugin.register :logo_header_plugin do
+  name 'UTN Logo Plugin'
+  author 'Nicolas Kovalsky'
+  description 'Agrega div en el header para el logo de UTN'
+  version '0.0.1'
+end
